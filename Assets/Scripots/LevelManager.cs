eb3e9a8a1d4524e58a163148686b9da7bec39e70
Shantanu Jamble <sj6045@rit.edu>
2018-12-15 18:14:27 -0500

    Added Undo feature, color notifications if move is valid or not

--- Assets/Scripots/LevelManager.cs
+++ Assets/Scripots/LevelManager.cs
@@ -2,7 +2,6 @@
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-
 public class LevelManager : MonoBehaviour {
 
 
@@ -23,7 +22,12 @@ public class LevelManager : MonoBehaviour {
     private GameObject[,] buttonGrid = new GameObject[9, 9];
     private int[,] sudokuGrid = new int[9, 9];
     private int currentNumber = 1;
-    
+
+
+    //Movement stack for undo feature.
+    Move last_move;
+    MovementStack moves = new MovementStack();
+
     //Time for the sudoku solving
 
     /// <summary>
@@ -31,6 +35,7 @@ public class LevelManager : MonoBehaviour {
     /// </summary>
     private void Start()
     {
+
         GenerateOptionsBoard();
         GenerateGameBoard(levelStrings[currentLevel]);
 
@@ -74,37 +79,114 @@ public class LevelManager : MonoBehaviour {
         RectTransform rectTransform;
         Text text;
         char[] tmp_input_array = null;
-        for (int i = 0; i < 9; i++)
+        for (int row_index = 0; row_index < 9; row_index++)
         {
-            tmp_input_array = inputLines[i].ToCharArray();
-            for (int j = 0; j < 9; j++)
+            tmp_input_array = inputLines[row_index].ToCharArray();
+            for (int col_index = 0; col_index < 9; col_index++)
             {
                 //Stops code from creating new buttons while switching levels
-                if (buttonGrid[i, j] == null)
+                if (buttonGrid[row_index, col_index] == null)
                 {
-                    buttonGrid[i, j] = Instantiate(buttonPrefab);
-                    tmp_button = buttonGrid[i, j].GetComponent<Button>();
-                    tmp_button.onClick.AddListener(Play);
+                    buttonGrid[row_index, col_index] = Instantiate(buttonPrefab);
+                    //gives us location of the button in terms of grid
+                    buttonGrid[row_index, col_index].name = row_index + "," + col_index;
                 }
-                rectTransform = buttonGrid[i, j].GetComponent<RectTransform>();
+                rectTransform = buttonGrid[row_index, col_index].GetComponent<RectTransform>();
                 rectTransform.SetParent(gameBoard.transform, false);
-                text = buttonGrid[i, j].GetComponentInChildren<Text>();
-                text.text = tmp_input_array[j] + "";
-                sudokuGrid[i,j] = (int)tmp_input_array[j];
 
-
+                tmp_button = buttonGrid[row_index, col_index].GetComponent<Button>();
+                tmp_button.onClick.AddListener(Play);
+                text = buttonGrid[row_index, col_index].GetComponentInChildren<Text>();
+                if (tmp_input_array[col_index] != '0')
+                    tmp_button.interactable = false;
+                text.text = tmp_input_array[col_index] + "";
+                sudokuGrid[row_index,col_index] = int.Parse(tmp_input_array[col_index]+"");
             }
         }
     }
 
+
+    /// <summary>
+    /// Action listners for the option buttons.
+    /// </summary>
     public void ChangeCurrentNumber()
     {
         currentNumber =int.Parse(EventSystem.current.currentSelectedGameObject.name);
         Debug.Log(currentNumber);
     }
 
+    /// <summary>
+    /// Action listner for the sudokuboard buttons.
+    /// </summary>
     public void Play()
     {
-        Debug.Log("You played");
+        GameObject gameObject = EventSystem.current.currentSelectedGameObject;
+        gameObject.GetComponentInChildren<Text>().text = currentNumber+"";
+        int row_index = int.Parse(gameObject.name.Split(',')[0]);
+        int col_index = int.Parse(gameObject.name.Split(',')[1]);
+        
+        
+        //Run check for the validity of move
+        if (IsValidMove(row_index, col_index))
+        {
+            gameObject.GetComponentInChildren<Text>().color = Color.green;
+            moves.Push(sudokuGrid[row_index, col_index], row_index, col_index, true);
+        }
+        else
+        {
+            gameObject.GetComponentInChildren<Text>().color = Color.red;
+            moves.Push(sudokuGrid[row_index, col_index], row_index, col_index, false);
+        }
+
+        sudokuGrid[row_index, col_index] = currentNumber;
+    }
+
+    /// <summary>
+    /// Checks for the move validity by checking the input with row,column and box.
+    /// </summary>
+    /// <param name="row_index"></param>
+    /// <param name="col_index"></param>
+    /// <returns></returns>
+    private bool IsValidMove(int row_index, int col_index)
+    {
+        Debug.Log("In here");
+        for (int counter = 0; counter < 9; counter++)
+        {
+            if (sudokuGrid[row_index, counter] == currentNumber || sudokuGrid[counter, col_index] == currentNumber)
+            {
+                return false;
+            }
+        }
+        //Check for mini square
+        int sq_start_row = (row_index / 3) * 3;
+        int sq_start_col = (col_index / 3) * 3;
+        for (int tmp_row_index = sq_start_row;tmp_row_index < sq_start_row + 3; tmp_row_index++)
+        {
+            for(int tmp_col_index = sq_start_col; tmp_col_index < sq_start_col + 3; tmp_col_index++)
+            {
+                Debug.Log(tmp_row_index + "  " + tmp_col_index+"  "+ sudokuGrid[tmp_row_index, tmp_col_index]);
+                if (sudokuGrid[tmp_row_index, tmp_col_index] == currentNumber)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+    public void Undo()
+    {
+        Debug.Log("in Undo");
+        last_move = moves.Pop();
+        if (last_move != null)
+        {
+            sudokuGrid[last_move.Row, last_move.Column] = last_move.Value;
+            buttonGrid[last_move.Row, last_move.Column].GetComponentInChildren<Text>().text = last_move.Value +"";
+            buttonGrid[last_move.Row, last_move.Column].GetComponentInChildren<Text>().color = (last_move.Value==0)?Color.black:
+                                                                            (last_move.IsValid)?Color.green:Color.red;
+            
+        }
     }
 }
---
+++ Assets/Scripots/MovementStack.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Move
+{
+    public int Row;
+    public int Column;
+    public int Value;
+    public bool IsValid;
+}
+
+/// <summary>
+/// Stack for tracking moves. Can be used for undo feature.
+/// </summary>
+public class MovementStack {
+
+    int stacktop;
+    Move[] moveArray;
+
+    public MovementStack() {
+        stacktop = -1;
+        moveArray= new Move[5];
+    }
+    
+    /// <summary>
+    /// Pushes the data of current move into array of moves.Maintains last five moves at max at any point of time.
+    /// </summary>
+    /// <param name="value">Last value at the cell</param>
+    /// <param name="row">Row of cell.</param>
+    /// <param name="col">Col of cell.</param>
+    /// <param name="isValid">Was the move valid.</param>
+    public void Push(int value,int row,int col,bool isValid=true)
+    {
+        Debug.Log("In push");
+        //Objects will be created only once and then we will keep resuing them so as we don't trigger garbahe collection.
+        if (stacktop == 4)
+        {
+            for(int index = 0; index < 4; index++)
+            { 
+                moveArray[index].Value = moveArray[index + 1].Value;
+                moveArray[index].Row = moveArray[index + 1].Row;
+                moveArray[index].Column = moveArray[index + 1].Column;
+                moveArray[index].IsValid = moveArray[index + 1].IsValid;
+            }
+            moveArray[4].Value = value;
+            moveArray[4].Row = row;
+            moveArray[4].Column = col;
+            moveArray[4].IsValid = isValid;
+        }
+        else
+        {
+            stacktop++;
+            if (moveArray[stacktop] == null)
+            {
+                moveArray[stacktop] = new Move();
+            }
+            moveArray[stacktop].Value = value;
+            moveArray[stacktop].Row = row;
+            moveArray[stacktop].Column = col;
+            moveArray[stacktop].IsValid = isValid;
+        }
+
+    }
+
+
+    /// <summary>
+    /// returns the latest move. Null if the array is empty.
+    /// </summary>
+    /// <returns></returns>
+    public Move Pop()
+    {
+        if (stacktop == -1)
+        {
+            return null;
+        }
+        else
+        {
+            return moveArray[stacktop--];
+        }
+    }
+	
+}
