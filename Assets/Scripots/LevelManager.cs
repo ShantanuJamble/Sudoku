2ed63838a0b875263ae9b811906254818aee46b5
Shantanu Jamble <sj6045@rit.edu>
2018-12-16 16:34:24 -0500

    Converted the system to Publisher Subscriber model

--- Assets/Scripots/LevelManager.cs
+++ Assets/Scripots/LevelManager.cs
@@ -1,9 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class LevelManager : MonoBehaviour {
 
+
+/// <summary>
+/// This is a listner which will be implemented by the level manager to listen from sudoku board.
+/// </summary>
+public interface IManager
+{
+    void MaintainMoves();
+}
+
+public class LevelManager : MonoBehaviour,IManager {
+
+    //SudokuBoard related data
+    SudokuBoard board = new SudokuBoard();
 
     // Setting up prefabs and links to game objets.
     [SerializeField]
@@ -13,39 +26,36 @@ public class LevelManager : MonoBehaviour {
     [SerializeField]
     private GameObject buttonPrefab;
     [SerializeField]
-    private GameObject ansButtonPrefab;
+    private GameObject optionsButtonPrefab;
 
 
     ///Level Related data
-    private string[] levelStrings = {"Level1", "Level2", "Level3" };
+    GameObject[,] buttonGrid = new GameObject[9, 9];
+    private string[] levelStrings = { "Level1", "Level2", "Level3" };
     private int currentLevel = 0;
-    private GameObject[,] buttonGrid = new GameObject[9, 9];
-    private int[,] sudokuGrid = new int[9, 9];
-    private int currentNumber = 1;
 
+    //Play related data
+    private int currentNumber = 1;
 
     //Movement stack for undo feature.
     Move last_move;
     MovementStack moves = new MovementStack();
-
-    //Time for the sudoku solving
 
     /// <summary>
     /// Default start method.
     /// </summary>
     private void Start()
     {
-
         GenerateOptionsBoard();
-        GenerateGameBoard(levelStrings[currentLevel]);
-
+        board.GenerateGameBoard(levelStrings[currentLevel],gameBoard,buttonPrefab,ref buttonGrid);
+        MaintainMoves();
     }
 
 
     /// <summary>
     /// Sets up the buttons on very right of window.
     /// </summary>
-    private void GenerateOptionsBoard()
+    public void GenerateOptionsBoard()
     {
         //Setup options board
         GameObject tmp_gameObject;
@@ -54,7 +64,7 @@ public class LevelManager : MonoBehaviour {
         Text text;
         for (int i = 1; i < 10; i++)
         {
-            tmp_gameObject = Instantiate(ansButtonPrefab);
+            tmp_gameObject = MonoBehaviour.Instantiate(optionsButtonPrefab);
             tmp_button = tmp_gameObject.GetComponent<Button>();
             tmp_button.name = i + "";
             tmp_button.onClick.AddListener(ChangeCurrentNumber);
@@ -67,126 +77,50 @@ public class LevelManager : MonoBehaviour {
 
 
     /// <summary>
-    /// Sets up level for us taking the file name.
-    /// </summary>
-    /// <param name="filename">String for the filename.</param>
-    private void GenerateGameBoard(string filename)
-    {
-        TextAsset data = Resources.Load(filename) as TextAsset;
-        Button tmp_button;
-        string[] inputLines = data.text.Split('\n');
-        Vector2 pos = new Vector2(-180f, 180f);
-        RectTransform rectTransform;
-        Text text;
-        char[] tmp_input_array = null;
-        for (int row_index = 0; row_index < 9; row_index++)
-        {
-            tmp_input_array = inputLines[row_index].ToCharArray();
-            for (int col_index = 0; col_index < 9; col_index++)
-            {
-                //Stops code from creating new buttons while switching levels
-                if (buttonGrid[row_index, col_index] == null)
-                {
-                    buttonGrid[row_index, col_index] = Instantiate(buttonPrefab);
-                    //gives us location of the button in terms of grid
-                    buttonGrid[row_index, col_index].name = row_index + "," + col_index;
-                }
-                rectTransform = buttonGrid[row_index, col_index].GetComponent<RectTransform>();
-                rectTransform.SetParent(gameBoard.transform, false);
-
-                tmp_button = buttonGrid[row_index, col_index].GetComponent<Button>();
-                tmp_button.onClick.AddListener(Play);
-                text = buttonGrid[row_index, col_index].GetComponentInChildren<Text>();
-                if (tmp_input_array[col_index] != '0')
-                    tmp_button.interactable = false;
-                text.text = tmp_input_array[col_index] + "";
-                sudokuGrid[row_index,col_index] = int.Parse(tmp_input_array[col_index]+"");
-            }
-        }
-    }
-
-
-    /// <summary>
     /// Action listners for the option buttons.
     /// </summary>
     public void ChangeCurrentNumber()
     {
-        currentNumber =int.Parse(EventSystem.current.currentSelectedGameObject.name);
+        currentNumber = int.Parse(EventSystem.current.currentSelectedGameObject.name);
         Debug.Log(currentNumber);
+        board.CurrentNumber= currentNumber;
     }
+
+
+    
+
 
     /// <summary>
-    /// Action listner for the sudokuboard buttons.
+    /// Implements the Undo functionality with the help of movement stack.
     /// </summary>
-    public void Play()
-    {
-        GameObject gameObject = EventSystem.current.currentSelectedGameObject;
-        gameObject.GetComponentInChildren<Text>().text = currentNumber+"";
-        int row_index = int.Parse(gameObject.name.Split(',')[0]);
-        int col_index = int.Parse(gameObject.name.Split(',')[1]);
-        
-        
-        //Run check for the validity of move
-        if (IsValidMove(row_index, col_index))
-        {
-            gameObject.GetComponentInChildren<Text>().color = Color.green;
-            moves.Push(sudokuGrid[row_index, col_index], row_index, col_index, true);
-        }
-        else
-        {
-            gameObject.GetComponentInChildren<Text>().color = Color.red;
-            moves.Push(sudokuGrid[row_index, col_index], row_index, col_index, false);
-        }
-
-        sudokuGrid[row_index, col_index] = currentNumber;
-    }
-
-    /// <summary>
-    /// Checks for the move validity by checking the input with row,column and box.
-    /// </summary>
-    /// <param name="row_index"></param>
-    /// <param name="col_index"></param>
-    /// <returns></returns>
-    private bool IsValidMove(int row_index, int col_index)
-    {
-        Debug.Log("In here");
-        for (int counter = 0; counter < 9; counter++)
-        {
-            if (sudokuGrid[row_index, counter] == currentNumber || sudokuGrid[counter, col_index] == currentNumber)
-            {
-                return false;
-            }
-        }
-        //Check for mini square
-        int sq_start_row = (row_index / 3) * 3;
-        int sq_start_col = (col_index / 3) * 3;
-        for (int tmp_row_index = sq_start_row;tmp_row_index < sq_start_row + 3; tmp_row_index++)
-        {
-            for(int tmp_col_index = sq_start_col; tmp_col_index < sq_start_col + 3; tmp_col_index++)
-            {
-                Debug.Log(tmp_row_index + "  " + tmp_col_index+"  "+ sudokuGrid[tmp_row_index, tmp_col_index]);
-                if (sudokuGrid[tmp_row_index, tmp_col_index] == currentNumber)
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-
-
     public void Undo()
     {
         Debug.Log("in Undo");
         last_move = moves.Pop();
         if (last_move != null)
         {
-            sudokuGrid[last_move.Row, last_move.Column] = last_move.Value;
-            buttonGrid[last_move.Row, last_move.Column].GetComponentInChildren<Text>().text = last_move.Value +"";
-            buttonGrid[last_move.Row, last_move.Column].GetComponentInChildren<Text>().color = (last_move.Value==0)?Color.black:
-                                                                            (last_move.IsValid)?Color.green:Color.red;
-            
+            board.SetCellValue(last_move.Row, last_move.Column, last_move.Value);
+            buttonGrid[last_move.Row, last_move.Column].GetComponentInChildren<Text>().text = last_move.Value + "";
+            buttonGrid[last_move.Row, last_move.Column].GetComponentInChildren<Text>().color = (last_move.Value == 0) ? Color.black :
+                                                                            (last_move.IsValid) ? Color.green : Color.red;
+
         }
     }
+
+
+    public void MaintainMoves()
+    {
+        board.MoveToStack += delegate
+        {
+            try
+            {
+                moves.Push(board.LastMove.Value, board.LastMove.Row, board.LastMove.Column, board.LastMove.IsValid);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Last move has invalid data or null value.");
+            }
+        };
+    }
+
 }
---
+++ Assets/Scripots/SudokuBoard.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+
+public delegate void PushMovesToStack();
+
+
+
+public class SudokuBoard
+{
+    private int[,] sudokuGrid = new int[9, 9];
+    private int currentNumber;
+    private Move lastMove = new Move();
+    //Listeners for the buttons on grid
+    public PushMovesToStack MoveToStack;
+
+
+
+    /// <summary>
+    /// Setter for the currently selected option. 
+    /// </summary>
+    public int CurrentNumber
+    {
+        set 
+        {
+            this.currentNumber = value;
+        }
+    }
+
+    /// <summary>
+    /// Getter for the last move played by user.
+    /// </summary>
+    public Move LastMove
+    {
+        get
+        {
+            return lastMove;
+        }
+    }
+
+
+    public void SetCellValue(int value,int row,int col)
+    {
+        try
+        {
+            sudokuGrid[row, col] = value;
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Invalid indices for sudokugrid.\n"+ e.Message);
+        }
+    }
+
+    /// <summary>
+    /// Sets up level for us taking the file name.
+    /// </summary>
+    /// <param name="filename">String for the filename.</param>
+    public void GenerateGameBoard(string filename,GameObject gameBoard,GameObject buttonPrefab,ref GameObject[,] buttonGrid)
+    {
+        TextAsset data = Resources.Load(filename) as TextAsset;
+        Button tmp_button;
+        string[] inputLines = data.text.Split('\n');
+        Vector2 pos = new Vector2(-180f, 180f);
+        RectTransform rectTransform;
+        Text text;
+        char[] tmp_input_array = null;
+        for (int row_index = 0; row_index < 9; row_index++)
+        {
+            tmp_input_array = inputLines[row_index].ToCharArray();
+            for (int col_index = 0; col_index < 9; col_index++)
+            {
+                //Stops code from creating new buttons while switching levels
+                if (buttonGrid[row_index, col_index] == null)
+                {
+                    buttonGrid[row_index, col_index] = MonoBehaviour.Instantiate(buttonPrefab);
+                    //gives us location of the button in terms of grid
+                    buttonGrid[row_index, col_index].name = row_index + "," + col_index;
+                }
+                rectTransform = buttonGrid[row_index, col_index].GetComponent<RectTransform>();
+                rectTransform.SetParent(gameBoard.transform, false);
+
+                tmp_button = buttonGrid[row_index, col_index].GetComponent<Button>();
+                tmp_button.onClick.AddListener(Play);
+                text = buttonGrid[row_index, col_index].GetComponentInChildren<Text>();
+                if (tmp_input_array[col_index] != '0')
+                    tmp_button.interactable = false;
+                text.text = tmp_input_array[col_index] + "";
+                sudokuGrid[row_index, col_index] = int.Parse(tmp_input_array[col_index] + "");
+            }
+        }
+    }
+
+
+    
+
+    /// <summary>
+    /// Action listner for the sudokuboard buttons.
+    /// </summary>
+    public void Play()
+    {
+        GameObject gameObject = EventSystem.current.currentSelectedGameObject;
+        gameObject.GetComponentInChildren<Text>().text = currentNumber + "";
+        int row_index = int.Parse(gameObject.name.Split(',')[0]);
+        int col_index = int.Parse(gameObject.name.Split(',')[1]);
+        lastMove.Value = sudokuGrid[row_index, col_index];
+        lastMove.Row = row_index;
+        lastMove.Column = col_index;
+    
+
+        //Run check for the validity of move
+        if (IsValidMove(row_index, col_index))
+        {
+            gameObject.GetComponentInChildren<Text>().color = Color.green;
+            lastMove.IsValid = true;
+        }
+        else
+        {
+            gameObject.GetComponentInChildren<Text>().color = Color.red;
+            lastMove.IsValid = false;
+        }
+        MoveToStack();
+        sudokuGrid[row_index, col_index] = currentNumber;
+
+    }
+
+
+    /// <summary>
+    /// Checks for the move validity by checking the input with row,column and box.
+    /// </summary>
+    /// <param name="row_index"></param>
+    /// <param name="col_index"></param>
+    /// <returns></returns>
+    private bool IsValidMove(int row_index, int col_index)
+    {
+        Debug.Log("In here");
+        for (int counter = 0; counter < 9; counter++)
+        {
+            if (sudokuGrid[row_index, counter] == currentNumber || sudokuGrid[counter, col_index] == currentNumber)
+            {
+                return false;
+            }
+        }
+        //Check for mini square
+        int sq_start_row = (row_index / 3) * 3;
+        int sq_start_col = (col_index / 3) * 3;
+        for (int tmp_row_index = sq_start_row; tmp_row_index < sq_start_row + 3; tmp_row_index++)
+        {
+            for (int tmp_col_index = sq_start_col; tmp_col_index < sq_start_col + 3; tmp_col_index++)
+            {
+                Debug.Log(tmp_row_index + "  " + tmp_col_index + "  " + sudokuGrid[tmp_row_index, tmp_col_index]);
+                if (sudokuGrid[tmp_row_index, tmp_col_index] == currentNumber)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+}
